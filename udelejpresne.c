b037230fd8de7f06d3359f67bf39705fb8608715
Tomas Janousek <tomi@nomi.cz>
2009-09-14 12:45:01 +0200

    Vice cest pod jednim zadanim uca/hesla.
    Signed-off-by: Tomas Janousek <tomi@nomi.cz>

--- udelejpresne.c
+++ udelejpresne.c
@@ -30,11 +30,6 @@ int main(int argc, char *argv[])
         abort();
     }
 
-    /* Cesta */
-    char path[512];
-    printf("Zadej cestu: ");
-    scanf("%511s", path);
-
     /* Cas */
     char cas[50], *cas_end;
     printf("Zadej cas: ");
@@ -61,66 +56,87 @@ int main(int argc, char *argv[])
     scanf("%d", &pocet_pokusu);
 
     pid_t pid = 1;
-    while (pid > 0 && pocet_pokusu-- > 1)
-        pid = fork();
 
-    /* Priprava debug filu. */
-    char df_name[128];
-    snprintf(df_name, 128, "brutalis-dbg-%s-XXXXXX", user);
-    int df_fd = mkstemp(df_name);
-    if (df_fd == -1)
-        perror("mkstemp"), abort();
-    FILE *df = fdopen(df_fd, "w");
-    if (!df)
-        perror("fdopen"), abort();
-    fprintf(df, "PATH: %s\n\n", path);
-    printf("\033[1mVystup jde do: %s\033[m\n", df_name);
-
-    brutalwait(t - 20000);
-
-    FILE *f = https_get("is.muni.cz", path);
-    if (f) {
-        http_auth_basic(f, user, pass);
-
-        brutalwait(t);
-
-        http_fire(f);
-
-        int ok = 0;
-
-        char cas[512] = "", *cas_p;
-        static const char cas_str[] = "Čas Informačního systému: <B>";
-
-        char buffer[4096];
-        while (fgets(buffer, 4096, f)) {
-            fputs(buffer, df);
-
-            if (strstr(buffer, "potvrzeni") || strstr(buffer, "varovani"))
-                ok = 1;
-
-            if ((cas_p = strstr(buffer, cas_str))) {
-                cas_p += strlen(cas_str);
-
-                char *end = strstr(cas_p, "</B>");
-                if (end)
-                    *end = 0;
-
-                strncpy(cas, cas_p, 512);
-                cas[511] = 0;
-            }
-
-            //fputs(buffer, stdout);
+    /* Cesty */
+    char path[512];
+    while (1) {
+        usleep(100000);
+        printf("Zadej cestu: ");
+        if (scanf("%511s", path) != 1) {
+            printf("Ukoncuji zadani. Vyckejte provedeni uloh.\n");
+            break;
         }
 
-        fclose(f);
-
-        if (!ok)
-            printf("%s: \033[1;31mCHYBA\033[m\n", cas);
-        else
-            printf("%s: \033[1;32mOK\033[m\n", cas);
+        /* Child nepokračuje ve smyčce */
+        if ((pid = fork()) == 0)
+            break;
     }
 
-    fclose(df);
+    /* Jen childi pracují */
+    if (pid == 0) {
+        /* Naforkovat na počet pokusů */
+        pid = 1;
+        while (pid > 0 && pocet_pokusu-- > 1)
+            pid = fork();
+
+        /* Priprava debug filu. */
+        char df_name[128];
+        snprintf(df_name, 128, "brutalis-dbg-%s-XXXXXX", user);
+        int df_fd = mkstemp(df_name);
+        if (df_fd == -1)
+            perror("mkstemp"), abort();
+        FILE *df = fdopen(df_fd, "w");
+        if (!df)
+            perror("fdopen"), abort();
+        fprintf(df, "PATH: %s\n\n", path);
+        printf("\033[1mVystup jde do: %s\033[m\n", df_name);
+
+        brutalwait(t - 20000);
+
+        FILE *f = https_get("is.muni.cz", path);
+        if (f) {
+            http_auth_basic(f, user, pass);
+
+            brutalwait(t);
+
+            http_fire(f);
+
+            int ok = 0;
+
+            char cas[512] = "", *cas_p;
+            static const char cas_str[] = "Čas Informačního systému: <B>";
+
+            char buffer[4096];
+            while (fgets(buffer, 4096, f)) {
+                fputs(buffer, df);
+
+                if (strstr(buffer, "potvrzeni") || strstr(buffer, "varovani"))
+                    ok = 1;
+
+                if ((cas_p = strstr(buffer, cas_str))) {
+                    cas_p += strlen(cas_str);
+
+                    char *end = strstr(cas_p, "</B>");
+                    if (end)
+                        *end = 0;
+
+                    strncpy(cas, cas_p, 512);
+                    cas[511] = 0;
+                }
+
+                //fputs(buffer, stdout);
+            }
+
+            fclose(f);
+
+            if (!ok)
+                printf("%s: \033[1;31mCHYBA\033[m\n", cas);
+            else
+                printf("%s: \033[1;32mOK\033[m\n", cas);
+        }
+
+        fclose(df);
+    }
     free(pass);
 
     /* Pockat na deti. */
